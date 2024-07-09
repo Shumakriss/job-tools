8cc288d8a558dd4998796c4e3fe7640f3cb27d71
Chris Shumaker <chris.shumaker@gmail.com>
2024-07-09 15:25:44 -0400

    Updated layout and links

--- site/modules/clipboard.js
+++ site/modules/clipboard.js
@@ -1,3 +1,5 @@
+const LINKEDIN_QUERY = "(software OR data) AND (founding OR senior OR principal OR staff OR L4 OR L5) AND (engineer OR architect)";
+
 function handleLinkedInClipboard() {
     navigator.clipboard.writeText("https://www.linkedin.com/in/christophershumaker/");
 }
@@ -8,4 +10,12 @@ function handleGithubClipboard() {
 
 function handleSiteClipboard() {
     navigator.clipboard.writeText("https://www.makerconsulting.llc/maker-consulting");
+}
+
+function handleLinkedInQueryClipboard() {
+    navigator.clipboard.writeText(LINKEDIN_QUERY);
+}
+
+function updateLinkedInQuery() {
+    document.getElementById("linkedin-query").innerHTML = LINKEDIN_QUERY;
 }
